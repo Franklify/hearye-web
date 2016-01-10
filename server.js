d9d3e1af39ce2f96c0040f28d01ab143e547eb3e
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-01-10 11:49:39 -0600

    get method to nodes about and team

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res) {
   res.sendFile(__dirname + '/views/about.html');
 });
+app.get('/team', function(req, res) {
+  res.sendFile(__dirname + '/views/team.html');
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
