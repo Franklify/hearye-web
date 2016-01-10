abcbb6b6c9dcfb79fecfb5ef5e2cfcfaf921157e
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-01-09 21:23:15 -0600

    changed variables

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
