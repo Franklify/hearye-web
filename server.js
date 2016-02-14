c9efbcee2ac3d5f74f917d610220df741f0f7654
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-02-13 20:07:39 -0600

    added 404 and 500 status

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.get('/blog', function(req, res) {
   res.sendFile(__dirname + '/views/blog.html');
 });
 
+//404 status handling
+app.use(function(request, response, next) {
+  response.status(404);
+  response.sendFile(path.join(__dirname + '/views/404.html'));
+});
+
+//Handle errors
+app.use(function(err, request, response, next) {
+  console.error('An application error has occurred:');
+  console.error(err.stack);
+  response.status(500);
+  response.sendFile(path.join(__dirname + '/views/500.html'));
+});
+
+
 app.listen(process.env.PORT || 1337, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
